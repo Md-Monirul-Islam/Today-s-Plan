aa5cdce00cdb7b2e7e094fdb123c2fdc9c9dde22
Md-Monirul-Islam <md.monirulmunna.cse@gmail.com>
2023-12-16 23:51:17 +0600

    body design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className='container-fluid my-5'>
+            <div className='row'>
+                <div className='col-sm-6 mx-auto'>
+                    <h2 className='text-center text-white shadow-lg p-3'>Today's Plan</h2>
+                    <div className='row'>
+                        <div className='col-9'>
+                        <input type="text" className='form-control' placeholder='Write your plan' />
+                        </div>
+                        <div className='col-2'>
+                            <button className='btn btn-warning px-5 fw-bold'>Add</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    )
+  }
 }
-
 export default App;
